e84db7c81ca86c7b7d555113a197556aab7eaedb
David Mohundro <david@mohundro.com>
2022-03-12 14:57:11 -0600

    Initial Commit

---
+++ swxmlhash-test/swxmlhash_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swxmlhash_testApp.swift
+//  swxmlhash-test
+//
+//  Created by David Mohundro on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct swxmlhash_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
